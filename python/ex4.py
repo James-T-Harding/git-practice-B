8d23e28d6828465305839fc9403a266c0795a6a1
jhard <j.harding26@outlook.com>
2023-07-18 14:23:21 +0100

    Typo

--- python/ex4.py
+++ python/ex4.py
@@ -1,6 +1,5 @@
-if __name__ == "__main__":
-    mark = input("Mark: ")
+mark = input("Mark: ")
 
-    if mark.isdigit():
-        mark_value = int(mark)
-        print("Distinction" if 85 < mark_value else "Pass" if 65 < mark_value < 85 else "Fail")
+if mark.isdigit():
+    mark_value = int(mark)
+    print("Distinction" if 85 < mark_value else "Pass" if 65 < mark_value < 85 else "Fail")
