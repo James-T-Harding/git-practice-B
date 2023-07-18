7583f945c57ce5ece5eb7dffb1ed530a3abb913b
jhard <j.harding26@outlook.com>
2023-07-18 12:14:40 +0100

    Did exercise 3

---
+++ python/ex3.py
@@ -0,0 +1,12 @@
+authors = {
+    "Casey Simmons": ["The wonderful world of os", "Return to the wonderful world of os"],
+    "Jarathteh Nolans": ["End times", "Final Times", "The End", "Last Stand"],
+    "Endas Newperson": ["My first book (please dont be mean)"]
+}
+
+
+if __name__ == "__main__":
+    author_name = input("Author name: ")
+
+    if books := authors.get(author_name):
+        print(", ".join(sorted(books)))
