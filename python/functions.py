a6c298bf8eaca080775f9993ebc14bbd0bca9d82
jhard <j.harding26@outlook.com>
2023-07-19 10:46:59 +0100

    Functions exercise

--- python/functions.py
+++ python/functions.py
@@ -1,12 +1,18 @@
 def take_grades(name, homework, assessment, final):
-    average = (homework + assessment + final) / 3
+    average = (4 * homework + 2 * assessment + final) / 3
+    grade_boundaries = [('A', 80), ('B', 70), ('C', 60), ('D', 50), ('F', 40)]
 
-    return f"{name} : {average:.2f}"
+    grade = 'A*'
 
+    for symbol, value in grade_boundaries:
+        if average < value:
+            grade = symbol
 
-prompts = []
+    return f"{name}: {average:.2f}% - {grade}"
+
 
 name = input("Name: ")
 scores = [input(f"{prompt} Score: ") for prompt in ("Homework", "Assessment", "Final")]
+result = take_grades(name, *map(int, scores))
 
-print(take_grades(name, *map(int, scores)))
+print(result)
---
+++ python/w3 - functions.py
@@ -0,0 +1,12 @@
+# 1
+def maximum(i1, i2, i3):
+    max_value = i1
+
+    for value in i2, i3:
+        if value > max_value:
+            max_value = value
+
+    return max_value
+
+# 2
+def summation(value_list):
